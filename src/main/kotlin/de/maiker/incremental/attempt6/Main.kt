a4b4fb6efbe4831b341ce4c0a387bb69dacbeff3
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-26 14:36:50 +0200

    use a reader class

--- src/main/kotlin/de/maiker/incremental/attempt6/Main.kt
+++ src/main/kotlin/de/maiker/incremental/attempt6/Main.kt
@@ -23,40 +23,39 @@ data class StationData(
     }
 }
 
-fun main() {
-    val name = "measurements"
-    val input = "./${name}.txt"
-    val output = "./${name}.out"
+val name = "measurements"
+val input = "./${name}.txt"
+val output = "./${name}.out"
 
-    val startTime = System.nanoTime()
+val SEMICOLON = 59.toByte() /* ';' */
+val DASH = 45.toByte() /* '-' */
+val NEWLINE = 10.toByte() /* \n */
+val DOT = 46.toByte() /* '.' */
+val ARRAY_BYTE_BASE_OFFSET = Unsafe.ARRAY_BYTE_BASE_OFFSET.toLong()
 
-    val unsafe = Unsafe::class.java.getDeclaredField("theUnsafe").let {
-        it.isAccessible = true
-        it.get(Unsafe::class) as Unsafe
-    }
+val unsafe = Unsafe::class.java.getDeclaredField("theUnsafe").let {
+    it.isAccessible = true
+    it.get(Unsafe::class) as Unsafe
+}
 
-    val channel = RandomAccessFile(input, "r").getChannel()
-    val memorySegment = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size(), Arena.ofShared())
+class Reader(
+    private val startAddress: Long,
+    private val endAddress: Long,
+) {
+    private var address = startAddress
+    private var byte: Byte = 0
 
-    val startAddress = memorySegment.address()
-    val endAddress = startAddress + memorySegment.byteSize()
-    var address = startAddress
+    fun hasRemaining(): Boolean = address < endAddress
 
-    var byte: Byte
-
-    val stations = mutableMapOf<Int, StationData>()
-
-    val SEMICOLON = 59.toByte() /* ';' */
-    val DASH = 45.toByte() /* '-' */
-    val NEWLINE = 10.toByte() /* \n */
-    val DOT = 46.toByte() /* '.' */
-
-    var nameLength = 0L
-    var hash: Int
+    private var nameStartAddress = 0L
+    private var nameLength = 0L
+    private var hash = 5381
     fun readName(): Int {
+        nameStartAddress = address
         nameLength = 0
         hash = 5381
-        while (true) { // no need to check, we will always hit the ';'
+        // no need to check, we will always hit the ';'
+        while (true) {
             byte = unsafe.getByte(address)
             address++
 
@@ -70,8 +69,14 @@ fun main() {
         return hash
     }
 
-    var isNegative: Boolean
-    var number: Int
+    private val nameBuffer = ByteArray(100)
+    fun getName(): String {
+        unsafe.copyMemory(null, nameStartAddress, nameBuffer, ARRAY_BYTE_BASE_OFFSET, nameLength)
+        return String(nameBuffer, 0, nameLength.toInt(), StandardCharsets.UTF_8)
+    }
+
+    private var isNegative = false
+    private var number = 0
     fun readNumber(): Int {
         isNegative = false
         number = 0
@@ -82,7 +87,8 @@ fun main() {
             address++
         }
 
-        while (true) { // no need to check, we will always hit the new line
+        // no need to check, we will always hit the new line
+        while (true) {
             byte = unsafe.getByte(address)
             address++
 
@@ -99,21 +105,26 @@ fun main() {
 
         return if (isNegative) -number else number
     }
+}
 
-    val ARRAY_BYTE_BASE_OFFSET = Unsafe.ARRAY_BYTE_BASE_OFFSET.toLong()
-    val nameBuffer = ByteArray(100)
-    var nameStartAddress: Long
-    while (address < endAddress) {
-        nameStartAddress = address
-        val hash = readName()
-        val value = readNumber()
+fun main() {
+    val startTime = System.nanoTime()
+
+    val channel = RandomAccessFile(input, "r").getChannel()
+    val memorySegment = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size(), Arena.ofShared())
+
+    val startAddress = memorySegment.address()
+    val endAddress = startAddress + memorySegment.byteSize()
+
+    val reader = Reader(startAddress, endAddress)
+    val stations = mutableMapOf<Int, StationData>()
+
+    while (reader.hasRemaining()) {
+        val hash = reader.readName()
+        val value = reader.readNumber()
 
         val stationData = stations.getOrPut(hash) {
-            unsafe.copyMemory(null, nameStartAddress, nameBuffer, ARRAY_BYTE_BASE_OFFSET, nameLength)
-
-            StationData(
-                name = String(nameBuffer, 0, nameLength.toInt(), StandardCharsets.UTF_8)
-            )
+            StationData(name = reader.getName())
         }
 
         if (value < stationData.min) { stationData.min = value }
